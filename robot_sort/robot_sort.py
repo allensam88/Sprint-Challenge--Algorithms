ea2ec7a205b10b17d9d16d40d0475dae178af693
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-20 12:57:08 -0700

    refactored robot sort to pass larger complicated tests

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,38 +100,64 @@ class SortingRobot:
         """
         # Utilize bubble sort methodology
         # swapping card values by comparing selected card with next card
+        # initialize the light to be on to start swap process
+        self.set_light_on()
+        while self.light_is_on():
+            # flip the light off again to wait for a swap to happen
+            self.set_light_off()
+            while self.can_move_right():
+                # pick up first or next item
+                self.swap_item()
+                # look at the next index item
+                self.move_right()
+                # if the item is bigger than the prior item, swap it
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    # indicate that a swap occurred
+                    self.set_light_on()
+                    # in all other cases, just put the item back in it's original spot
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+            # once you reach the end, go all the way back to the beginning
+            while self.can_move_left():
+                self.move_left()
 
+
+"""
+-----Original Naive Solution-----
+	def sort(self):
         self.set_light_on()
         while self.light_is_on():
             self.set_light_off()
             while self.can_move_right():
-                # print(self._list)
-                self.swap_item()
-                # print(f'Pick up card: {self._item}')
-                # print(self._list)
-                self.move_right()
-                # print(f'Look at card in next index: {self._position}')
-                if self.compare_item() < 0:
-                    # print(
-                    #     'If the card is smaller, return selection to original spot')
-                    self.move_left()
-                    # print('Go back')
-                    self.swap_item()
-                    # print(f'Drop card selection')
-                    self.move_right()
-                elif self.compare_item() >= 0:
-                    # print('If the card is larger, swap out')
-                    self.swap_item()
-                    # print(f'Swap and pick up new card: {self._item}')
-                    self.move_left()
-                    # print(f'Place card in prior spot: {self._position}')
-                    self.swap_item()
-                    # print(f'Drop card selection')
-                    self.move_right()
-                    # print('Move to next card')
-                    self.set_light_on()
-            while self.can_move_left():
-                self.move_left()
+				print(self._list)
+        		self.swap_item()
+        		print(f'Pick up card: {self._item}')
+        		print(self._list)
+        		self.move_right()
+        		print(f'Look at card in next index: {self._position}')
+        		if self.compare_item() < 0:
+            		print('If the card is smaller, return selection to original spot')
+            		self.move_left()
+            		print('Go back')
+            		self.swap_item()
+            		print(f'Drop card selection')
+            		self.move_right()
+        		elif self.compare_item() >= 0:
+            		print('If the card is larger, swap out')
+            		self.swap_item()
+            		print(f'Swap and pick up new card: {self._item}')
+            		self.move_left()
+            		print(f'Place card in prior spot: {self._position}')
+            		self.swap_item()
+            		print(f'Drop card selection')
+            		self.move_right()
+            		print('Move to next card')
+            		self.set_light_on()
+        	while self.can_move_left():
+            	self.move_left()
+"""
 
 
 if __name__ == "__main__":
